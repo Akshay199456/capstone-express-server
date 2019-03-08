b230e0681d4ac6064a4e86f0898508a055d0047b
Akshay199456 <akshay.kum94@gmail.com>
2019-03-07 19:48:11 -0800

    Completed Login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
+// Logout route
+/*
+router.get('/logout', (req, res) => {
+	console.log("Logout req session: ",req.session);
+    req.session.destroy((err) => {
+    	if(err){
+    	  	res.status(400).json({
+      			error: err
+      		});
+    	}
+
+	    else {
+	    	res.status(200).json({
+	    		success: 'logout successful'
+	    	});
+	    }
+	});
+});
+*/
 
 router.get('/', async(req, res) => {
 	res.status(200).json({
@@ -11,11 +30,56 @@ router.get('/', async(req, res) => {
 });
 
 
+// Login route
 router.post('/login', async(req, res) =>{
 	console.log("Login route hit");
+	console.log("Req body from login route: ", req.body);
+	try{
+		const foundCount = await User.countDocuments({ email: req.body.email})
+		if(foundCount === 0){
+			res.status(400).json({
+		    	error: 'Incorrect email entered. Please enter a valid email if you have registered else register an account.',
+		    	userCount: foundCount
+			});
+		}
+		else{
+			console.log("User count: ", foundCount);
+			const foundUser = await User.findOne({ email: req.body.email})
+			if (bcrypt.compareSync(req.body.password, foundUser.password)){
+
+				req.session.logged = true;
+			    req.session.email = foundUser.email;
+			    req.session.userId = foundUser._id;
+
+			    const sessionObject = {};
+			    sessionObject.logged = req.session.logged;
+			    sessionObject.email = req.session.email;
+			    sessionObject.userId = req.session.userId;
+
+				res.status(200).json({
+					success: 'Successfully logged in!',
+					session: sessionObject
+				});
+			}
+
+			else{
+				res.status(400).json({
+			    	error: 'Incorrect password entered. Please enter a valid password.'
+				});
+			}
+		}
+	}
+
+	catch(err){
+		console.log(err);
+		res.status(400).json({
+		    error: err
+		});
+	}
 });
 
 
+// Register route
 router.post('/register', async(req, res) =>{
 	console.log('Register route hit!');
 	console.log("Req body: ", req.body);
@@ -49,7 +113,7 @@ router.post('/register', async(req, res) =>{
 
 		  catch(err){
 		    console.log(err);
-		    res.status(400).send({
+		    res.status(400).json({
 		    	error: 'Email already exists. Please enter a new email to register an account'
 		    });
 		  }
