2bcf0e3a0fb38235942e28d28a5e4b09e9d037b5
Akshay199456 <akshay.kum94@gmail.com>
2019-03-07 16:15:49 -0800

    Completed register route

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 
 
 router.get('/', async(req, res) => {
-	res.status(400).json({
+	res.status(200).json({
 		message: 'sending data'
 	});
 });
@@ -18,7 +18,48 @@ router.post('/login', async(req, res) =>{
 
 router.post('/register', async(req, res) =>{
 	console.log('Register route hit!');
-	
+	console.log("Req body: ", req.body);
+
+	if(req.body.password === req.body.verify_password){
+		const password = req.body.password;
+		const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+		const userDbEntry = {};
+		userDbEntry.username = req.body.username;
+		userDbEntry.password = hashedPassword;
+		userDbEntry.email = req.body.email;
+		try {
+
+		    const user = await User.create(userDbEntry);
+		    console.log("Registered user: ", user);
+
+		    req.session.logged = true;
+		    req.session.email = req.body.email;
+		    req.session.userId = user._id;
+
+		    const sessionObject = {};
+		    sessionObject.logged = req.session.logged;
+		    sessionObject.email = req.session.email;
+		    sessionObject.userId = req.session.userId;
+
+		    res.status(200).json({
+		      success: 'User successfully registered',
+		      session: sessionObject
+		    });
+		  } 
+
+		  catch(err){
+		    console.log(err);
+		    res.status(400).send({
+		    	error: 'Email already exists. Please enter a new email to register an account'
+		    });
+		  }
+	}
+
+	else{
+		res.status(400).json({
+			error: 'Password and Verify Password don\'t match. Please make sure they are the same'
+		})
+	}
 });
 
 
--- models/user.js
+++ models/user.js
@@ -5,14 +5,30 @@ const userSchema = mongoose.Schema();
 
 userSchema.add({
   username: {
-  	type: String, 
-  	unique: true
+  	type: String
   },
   password: String,
   email: {
   	type:String,
   	unique: true
   },
+
+  // Users who the current user is following
+  followingUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+
+  // Users who are following the current user
+  followedByUsers: [{
+  	type: Schema.Types.ObjectId,
+  	ref: 'User'
+  }],
+  searchQuery: {
+  	type:String,
+  	default: ''
+  },
+  searchHistory: [String]
 });
 
 
