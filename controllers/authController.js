4298dd8bf529086443848a7d52fb703057f5e966
Akshay199456 <akshay.kum94@gmail.com>
2019-03-07 20:07:03 -0800

    Added regex testing standard to register route

--- controllers/authController.js
+++ controllers/authController.js
@@ -84,46 +84,76 @@ router.post('/register', async(req, res) =>{
 	console.log('Register route hit!');
 	console.log("Req body: ", req.body);
 
-	if(req.body.password === req.body.verify_password){
-		const password = req.body.password;
-		const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-		const userDbEntry = {};
-		userDbEntry.username = req.body.username;
-		userDbEntry.password = hashedPassword;
-		userDbEntry.email = req.body.email;
-		try {
+	const usernameRegex = /^[\w]{3,10}$/
+    const passwordRegex = /^[\w]{3,10}$/
 
-		    const user = await User.create(userDbEntry);
-		    console.log("Registered user: ", user);
+    const usernameTest = usernameRegex.test(req.body.username);
+    const passwordTest = passwordRegex.test(req.body.password);
+    const verifyPasswordTest = passwordRegex.test(req.body.verify_password);
 
-		    req.session.logged = true;
-		    req.session.email = req.body.email;
-		    req.session.userId = user._id;
+    if(usernameTest && passwordTest && verifyPasswordTest){
+		if(req.body.password === req.body.verify_password){
+			const password = req.body.password;
+			const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+			const userDbEntry = {};
+			userDbEntry.username = req.body.username;
+			userDbEntry.password = hashedPassword;
+			userDbEntry.email = req.body.email;
+			try {
 
-		    const sessionObject = {};
-		    sessionObject.logged = req.session.logged;
-		    sessionObject.email = req.session.email;
-		    sessionObject.userId = req.session.userId;
+			    const user = await User.create(userDbEntry);
+			    console.log("Registered user: ", user);
 
-		    res.status(200).json({
-		      success: 'User successfully registered',
-		      session: sessionObject
-		    });
-		  } 
+			    req.session.logged = true;
+			    req.session.email = req.body.email;
+			    req.session.userId = user._id;
 
-		  catch(err){
-		    console.log(err);
-		    res.status(400).json({
-		    	error: 'Email already exists. Please enter a new email to register an account'
-		    });
-		  }
+			    const sessionObject = {};
+			    sessionObject.logged = req.session.logged;
+			    sessionObject.email = req.session.email;
+			    sessionObject.userId = req.session.userId;
+
+			    res.status(200).json({
+			      success: 'User successfully registered',
+			      session: sessionObject
+			    });
+			  } 
+
+			  catch(err){
+			    console.log(err);
+			    res.status(400).json({
+			    	error: 'Email already exists. Please enter a new email to register an account'
+			    });
+			  }
+		}
+
+		else{
+			res.status(400).json({
+				error: 'Password and Verify Password don\'t match. Please make sure they are the same'
+			})
+		}
 	}
 
-	else{
-		res.status(400).json({
-			error: 'Password and Verify Password don\'t match. Please make sure they are the same'
-		})
-	}
+	// If username does not meet regex standard
+    else if(!usernameTest){
+      res.status(400).json({
+          error: "Username does not meet standard. Username must be alphanumeric with atleast 3 and atmost 10 characters" 
+        });
+    }
+
+    // If password does not meet regex standard
+    else if(!passwordTest){
+      res.status(400).json({
+          error: "Password does not meet standard. Password must be alphanumeric with atleast 3 and atmost 10 characters" 
+        });
+    }
+
+    // If verify_password does not meet regex standard
+    else if(!verifyPasswordTest){
+      res.status(400).json({
+          error: "Verify Password does not meet standard. Verify Password must be alphanumeric with atleast 3 and atmost 10 characters"
+        });
+    }
 });
 
 
